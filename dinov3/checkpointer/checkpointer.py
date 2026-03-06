6523b7493e916324eafef60070b148f85e9d47ff
Gabriel Tsai <gabrielhtsai@gmail.com>
2026-03-06 13:07:31 -0800

    actual strict=False

--- dinov3/checkpointer/checkpointer.py
+++ dinov3/checkpointer/checkpointer.py
@@ -275,7 +275,7 @@ def init_fsdp_model_from_checkpoint(
     if not Path(checkpoint_path).is_dir():  # PyTorch standard checkpoint
         logger.info(f"Loading pretrained weights from {checkpoint_path}")
         if ("sat" in Path(checkpoint_path).name):
-            raw = torch.load(checkpoint_path, map_location="cpu", strict=False)
+            raw = torch.load(checkpoint_path, map_location="cpu")
             chkpt = {f"backbone.{k}": v for k, v in raw.items()}
         else:
             chkpt = torch.load(checkpoint_path, map_location="cpu")["teacher"]
@@ -302,7 +302,8 @@ def init_fsdp_model_from_checkpoint(
                 key: tensor
                 for key, tensor in chkpt.items()
                 if not any(skip_load_key in key for skip_load_key in skip_load_keys)
-            }
+            }, 
+            strict=False
         )
     else:  # DCP checkpoint
         load_checkpoint(ckpt_dir=checkpoint_path, model=model, process_group=process_group)
